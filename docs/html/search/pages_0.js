4d68b23b03e7ed9953d6e58607256dcb2d0550d0
佐藤順子 <testkusa@gmail.com>
2020-06-25 08:16:57 +0900

    ドキュメント変更

---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['test',['test',['../md__c_1__users_link_source_mapmake_vc2019_readme.html',1,'']]]
+];
--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -2,20 +2,23 @@ var indexSectionsWithContent =
 {
   0: "abefglmntw",
   1: "abefglmntw",
-  2: "m"
+  2: "m",
+  3: "t"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "files"
+  2: "files",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "全て",
   1: "クラス",
-  2: "ファイル"
+  2: "ファイル",
+  3: "ページ"
 };
 
